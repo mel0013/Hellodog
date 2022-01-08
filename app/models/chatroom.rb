faa584d357348e5b2da08bacfe349247ab9b242f
Melinda Batang <meeelinda.b@gmail.com>
2022-01-08 17:45:00 +1100

    completed chatrooms and messages controller. Created green-header CSS class. Chatroom index/show in progress

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   has_many :messages
   belongs_to :user1, class_name: "User", foreign_key: :user1_id
   belongs_to :user2, class_name: "User", foreign_key: :user2_id
+
+  def self.existing_chatroom_for(user1, user2)
+    Chatroom.where(user1: user1, user2: user2).or(Chatroom.where(user1: user2, user2: user1)).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :chatrooms, only: :create
+  end
   resources :dogs
-  resources :chatrooms, only: [ :show, :create ] do
+  resources :chatrooms, only: [:show, :index] do
     resources :messages, only: :create
   end
 end
