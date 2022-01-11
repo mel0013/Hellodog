789f1551e00b9ff9990228882723dbaa8c07cdfa
Mardina Shields <mardina.shields@gmail.com>
2022-01-11 20:55:19 +1100

    merged to master

--- db/schema.rb
+++ db/schema.rb
@@ -60,24 +60,6 @@ ActiveRecord::Schema.define(version: 2022_01_08_015556) do
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
-  create_table "favorites", force: :cascade do |t|
-    t.string "favoritable_type", null: false
-    t.bigint "favoritable_id", null: false
-    t.string "favoritor_type", null: false
-    t.bigint "favoritor_id", null: false
-    t.string "scope", default: "favorite", null: false
-    t.boolean "blocked", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["blocked"], name: "index_favorites_on_blocked"
-    t.index ["favoritable_id", "favoritable_type"], name: "fk_favoritables"
-    t.index ["favoritable_type", "favoritable_id", "favoritor_type", "favoritor_id", "scope"], name: "uniq_favorites__and_favoritables", unique: true
-    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable_type_and_favoritable_id"
-    t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
-    t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor_type_and_favoritor_id"
-    t.index ["scope"], name: "index_favorites_on_scope"
-  end
-
   create_table "invitations", force: :cascade do |t|
     t.string "status", default: "pending"
     t.string "address"
