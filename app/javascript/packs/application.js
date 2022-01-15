881dc34a7196ec22d4bde4ee2abf69cd3dc75abc
Elana Strijbis <ehgestrijbis@gmail.com>
2022-01-15 12:06:47 +1100

    centered cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,7 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
+
   // Call your functions here, e.g:
   // initSelect2();
 });
