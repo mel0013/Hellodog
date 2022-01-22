d76ddabd8e9c5210da96b479c47f4422278877fe
Elana Strijbis <ehgestrijbis@gmail.com>
2022-01-22 17:31:17 +1100

    deleted folders

--- SLACK/app/models/invitation.rb
+++
@@ -1,4 +0,0 @@
-class Invitation < ApplicationRecord
-  belongs_to :user1, class_name: "User", foreign_key: :user1_id
-  belongs_to :user2, class_name: "User", foreign_key: :user2_id
-end
--- SLACK/db/migrate/20211217105620_add_first_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddFirstNameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :first_name, :string, null: false, default: ""
-  end
-end
